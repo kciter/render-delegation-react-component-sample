b5c5a701a3821d7ea2f8af7c3df00284d64f8361
Lee Sun-Hyoup <kciter@naver.com>
2024-01-11 18:38:27 +0900

    Add sample code

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
-function App() {
-  return <div />;
-}
+import { Button } from "./components/Button";
+
+const App = () => {
+  return (
+    <div>
+      <Button>
+        <a href="https://www.google.com">Google</a>
+      </Button>
+      <Button asChild>
+        <a href="https://www.google.com">Google</a>
+      </Button>
+    </div>
+  );
+};
 
 export default App;
